9aca5b1bc8544ef074f5774b0058d624c7c1ee3e
foo@b.ar <info@127.0.0.1>
2026-07-04 08:36:51 +0200

    2026-07-04 08:36:51 +0200

--- fire.sh
+++ fire.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-for repo in ./*; do
+for repo in ./* ./CONTAINER/*; do
   [ -d "$repo/.git" ] || continue
   [[ "$repo" == ./3rd-party ]] && continue
   git -C "$repo" add -A; git -C "$repo" diff --cached --quiet && { echo "[$(basename "$repo")] unchanged - skipped"; continue; }
   git -C "$repo" commit -m "$(date '+%Y-%m-%d %H:%M:%S %z')" || continue
   git -C "$repo" push || continue
-  [[ "$repo" == ./CONTAINER ]] || [ ! -f "$repo/tag.sh" ] || (cd "$repo" && ./tag.sh)
+  [[ "$repo" == ./CONTAINER* ]] || [ ! -f "$repo/tag.sh" ] || (cd "$repo" && ./tag.sh)
 done
